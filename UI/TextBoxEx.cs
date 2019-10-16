a3a43504569e0977669bfbfddc4b8f8891cb0ad6
Hungfu Lee <hungfu.lee@insydesw.com.tw>
2019-10-16 16:24:34 +0800

    Enable textbox undo/redo

--- UI/TextBoxEx.cs
+++ UI/TextBoxEx.cs
@@ -14,6 +14,125 @@ namespace Utilities.UI
         Locked<Color> mNormalBackColor = Color.FromArgb(255, 255, 255, 255);
         Locked<Color> mFocusBackColor = Color.FromArgb(255, 255, 255, 128);
         volatile bool Focused = false;
+        UndoRedoStack<String> TextBoxUndoRedoTracker = new UndoRedoStack<string>();
+
+        public event EventHandler<bool> OnCanUndoChanged
+        {
+            add
+            {
+                TextBoxUndoRedoTracker.OnCanRedoChanged += value;
+            }
+            remove
+            {
+                TextBoxUndoRedoTracker.OnCanRedoChanged += value;
+            }
+        }
+        public event EventHandler<bool> OnCanRedoChanged
+        {
+            add
+            {
+                TextBoxUndoRedoTracker.OnCanRedoChanged += value;
+            }
+            remove
+            {
+                TextBoxUndoRedoTracker.OnCanRedoChanged += value;
+            }
+        }
+        /// <summary>
+        /// frequency to track undo/redo, default to 100 ms
+        /// </summary>
+        [Description("Frequency to track text change")]
+        public int TrackFrequency
+        {
+            get
+            {
+                return TextBoxUndoRedoTracker.TrackFrequency;
+            }
+            set
+            {
+                TextBoxUndoRedoTracker.TrackFrequency = value;
+            }
+        }
+
+        /// <summary>
+        /// Track text change
+        /// </summary>
+        [Description("是否追蹤文字變動到堆疊(預設:false)")]
+        public bool IsTextChangeTracked
+        {
+            get
+            {
+                return TextBoxUndoRedoTracker.IsTextChangeTracked;
+            }
+            set
+            {
+                TextBoxUndoRedoTracker.IsTextChangeTracked = value;
+                if (value)
+                {
+                    TextBoxUndoRedoTracker.PerformPushChange(this.Text);
+                }
+            }
+        }
+
+
+        public bool CanUndo
+        {
+            get
+            {
+                return TextBoxUndoRedoTracker.CanRedo;
+            }
+        }
+        public bool CanRedo
+        {
+            get
+            {
+                return TextBoxUndoRedoTracker.CanRedo;
+            }
+        }
+
+
+        public void Redo()
+        {
+            TextBoxUndoRedoTracker.Redo();
+        }
+        public void Undo()
+        {
+            TextBoxUndoRedoTracker.Undo();
+        }
+     
+        
+        
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            TextBoxUndoRedoTracker.PerformPushChange(this.Text);
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (ModifierKeys.HasFlag(Keys.Control))
+            {
+                if (keyData.HasFlag( Keys.Z))
+                {
+                    if (ModifierKeys.HasFlag(Keys.Shift))
+                    {
+                        Redo();
+                    }
+                    else
+                    {
+                        Undo();
+                    }
+                    return true;
+                }
+                else if (keyData.HasFlag(Keys.Y))
+                {
+                    Redo();
+                    return true;
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        
         [Browsable(true)]
         public Color MouseEnterBackColor
         {
@@ -54,7 +173,13 @@ namespace Utilities.UI
         }
         public TextBoxEx()
         {
+            TextBoxUndoRedoTracker.OnUndoRedo += TextBoxUndoRedoTracker_OnUndoRedo;
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        void TextBoxUndoRedoTracker_OnUndoRedo(object sender, string e)
+        {
+            this.Text= e;
         }
         protected override void OnGotFocus(EventArgs e)
         {
---
+++ UndoRedoStack.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class UndoRedoStack<T>
+    {
+        bool doTrackTextChange = true;
+        List<T> StatusStack = new List<T>();
+        int StackPoint = -1;
+        int m_TrackFrequency = 100;
+        bool TrackUndoRedo = true;
+        DateTime TextChangeTime;
+        public event EventHandler<T> OnUndoRedo;
+        public event EventHandler<bool> OnCanUndoChanged;
+        public event EventHandler<bool> OnCanRedoChanged;
+        /// <summary>
+        /// frequency to track undo/redo, default to 100 ms
+        /// </summary>
+        [Description("Frequency to track text change")]
+        public int TrackFrequency
+        {
+            get
+            {
+                return m_TrackFrequency;
+            }
+            set
+            {
+                m_TrackFrequency = value;
+            }
+        }
+
+        /// <summary>
+        /// Track text change
+        /// </summary>
+        [Description("是否追蹤文字變動到堆疊(預設:false)")]
+        public bool IsTextChangeTracked
+        {
+            get
+            {
+                return doTrackTextChange;
+            }
+            set
+            {
+                doTrackTextChange = value;
+            }
+        }
+
+        bool m_CanUndo;
+        bool m_CanRedo;
+
+        public bool CanUndo
+        {
+            get
+            {
+                return m_CanUndo;
+               
+            }
+            set
+            {
+                bool oldValue = m_CanUndo;
+                m_CanUndo = value;
+                if (oldValue != m_CanUndo)
+                {
+                    if (OnCanRedoChanged != null)
+                    {
+                        OnCanRedoChanged(this, value);
+                    }
+                }
+            }
+        }
+        public bool CanRedo
+        {
+            get
+            {
+                return m_CanRedo;  
+            }
+            set
+            {
+                bool oldValue = m_CanRedo;
+                m_CanRedo = value;
+                if (oldValue != m_CanRedo)
+                {
+                    if (OnCanRedoChanged != null)
+                    {
+                        OnCanRedoChanged(this, value);
+                    }
+                }
+            }
+        }
+        private bool CheckCanUndo()
+        {
+            return StackPoint > 0;
+        }
+        private bool CheckCanRedo()
+        {
+            return StackPoint + 1 < StatusStack.Count;
+        }
+
+        public void Redo()
+        {
+            TrackUndoRedo = false;
+            CanRedo = CheckCanRedo();
+            if (!CanRedo)
+            {
+                TrackUndoRedo = true;
+                return;
+            }
+            T content = StatusStack[StackPoint + 1];
+            ++StackPoint;
+            if (OnUndoRedo != null)
+            {
+                OnUndoRedo(this, content);
+            }
+            CanUndo = CheckCanUndo();
+            CanRedo = CheckCanRedo();
+            TrackUndoRedo = true;
+        }
+        public void Undo()
+        {
+            TrackUndoRedo = false;
+            CanUndo = CheckCanUndo();
+            if (!CanUndo)
+            {
+                TrackUndoRedo = true;
+                return;
+            }
+            T content = StatusStack[StackPoint - 1];
+            --StackPoint;
+            if (OnUndoRedo != null)
+            {
+                OnUndoRedo(this, content);
+            }
+            CanUndo = CheckCanUndo();
+            CanRedo = CheckCanRedo();
+            TrackUndoRedo = true;
+        }
+        public void PerformPushChange(T content)
+        {
+            if (doTrackTextChange)
+            {
+                if (!TrackUndoRedo)
+                {
+                    return;
+                }
+                DateTime now = DateTime.Now;
+                if (now.Subtract(TextChangeTime).TotalMilliseconds < m_TrackFrequency)
+                {
+                    return;
+                }
+                TextChangeTime = now;
+                if (StackPoint != -1 && StackPoint != StatusStack.Count - 1)
+                {
+                    StatusStack.RemoveRange(StackPoint, (StatusStack.Count - StackPoint));
+                }
+                StatusStack.Add(content);
+                ++StackPoint;
+                CanUndo = CheckCanUndo();
+                CanRedo = CheckCanRedo();
+            }
+        }
+    }
+}
