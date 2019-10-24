1a5a3f6f0411ada9d9670dd6b03fcb17f75ba48d
sunneo <sunneo529@gmail.com>
2019-10-25 02:40:08 +0800

    add doubleBufferedTab

---
+++ UI/DoubleBufferedTabPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utilities.UI
+{
+    public class DoubleBufferedTabPage:TabPage
+    {
+        public DoubleBufferedTabPage()
+        {
+            DoubleBuffered = true;
+        }
+    }
+    public class DoubleBufferedTabControl : TabControl
+    {
+        public DoubleBufferedTabControl()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+    }
+}
