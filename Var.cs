b3bfc26563b5c22ec60666d55e2154bc850415d2
sunneo <sunneo529@gmail.com>
2019-10-31 00:42:03 +0800

    add chain function

--- Var.cs
+++ Var.cs
@@ -6,27 +6,78 @@ using System.Threading.Tasks;
 
 namespace Utilities
 {
-    public class Var<T>
+    public class BaseVar
     {
-        T m_Value = default(T);
-        public event EventHandler<T> ValueChanged;
+        public class ChainActionArgs
+        {
+            public BaseVar pthis;
+            public BaseVar[] others;
+        }
+        public event EventHandler BaseValueChanged;
         public bool HasValue
         {
             get;
-            private set;
+            internal set;
         }
         public bool Changed
         {
             get;
-            private set;
+            internal set;
         }
+
+        public BaseVar Chain(BaseVar pthis, Action<ChainActionArgs> action, params BaseVar[] others)
+        {
+            EventHandler handler = new EventHandler((s, e) =>
+            {
+                if (others.Length > 0)
+                {
+                    bool hasValue = true;
+                    foreach (BaseVar other in others)
+                    {
+                        if (!other.HasValue)
+                        {
+                            hasValue = false;
+                            break;
+                        }
+                    }
+                    if (hasValue && action != null)
+                    {
+                        ChainActionArgs args = new ChainActionArgs();
+                        args.pthis = pthis;
+                        args.others = others;
+                        action(args);
+                    }
+                }
+            });
+            foreach (BaseVar other in others)
+            {
+                other.BaseValueChanged += handler;
+            }
+            return pthis;
+        } 
+        protected virtual void NotifyBaseValueChanged()
+        {
+            if (BaseValueChanged != null)
+            {
+                BaseValueChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+    public class Var<T>:BaseVar
+    {
+        T m_Value = default(T);
+        public event EventHandler<T> ValueChanged;
+        public bool ValueChangeTriggerOnlyOnChange = false;
+
         protected virtual void NotifyValueChanged()
         {
             if (ValueChanged != null)
             {
-                ValueChanged(this, Value);
+                ValueChanged(this, m_Value);
             }
+            NotifyBaseValueChanged();
         }
+       
         public T Value
         {
             get
@@ -36,10 +87,21 @@ namespace Utilities
             }
             set
             {
+                T origVal = m_Value;
                 m_Value = value;
                 Changed = true;
                 HasValue = true;
-                NotifyValueChanged();
+                if (ValueChangeTriggerOnlyOnChange)
+                {
+                    if (!m_Value.Equals(origVal))
+                    {
+                        NotifyValueChanged();
+                    }
+                }
+                else
+                {
+                    NotifyValueChanged();
+                }
             }
         }
         public Var()
