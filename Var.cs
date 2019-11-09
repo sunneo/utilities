0106a1c55254ad9ef4301f91d53916d7c51ee78b
sunneo <sunneo529@gmail.com>
2019-11-09 17:26:23 +0800

    Add VarStringPipe to accept string and transform to value type and write to target

--- Var.cs
+++ Var.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities.OptionParser.OptionParse;
 
 namespace Utilities
 {
@@ -63,6 +64,11 @@ namespace Utilities
             }
         }
     }
+
+    /// <summary>
+    /// a var data type which listen to value change
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Var<T>:BaseVar
     {
         T m_Value = default(T);
@@ -125,5 +131,52 @@ namespace Utilities
             return Value.ToString();
         }
        
+    }
+
+    /// <summary>
+    /// pipe a string to a target Var type
+    /// target type should implement TryParse 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class VarStringPipe<T> : Var<String>
+    {
+        public Var<String> Source = new Var<string>();
+        public Var<T> Target;
+        protected override void NotifyValueChanged()
+        {
+            base.NotifyValueChanged();
+            T value;
+            if (DynamicTryParse<T>.TryParse(this.Value, out value))
+            {
+                Target.Value = value;
+            }
+        }
+        /// <summary>
+        /// sync this string value to target
+        /// </summary>
+        /// <param name="target"></param>
+        public VarStringPipe(Var<T> target)
+        {
+            this.Target = target;
+        }
+        /// <summary>
+        /// sync a string to this and transform to target
+        /// </summary>
+        /// <param name="source">string source</param>
+        /// <param name="target">transform target</param>
+        public VarStringPipe(Var<String> source,Var<T> target)
+        {
+            this.Source = source;
+            if (this.Source != null)
+            {
+                this.Source.ValueChanged += Source_ValueChanged;
+            }
+            this.Target = target;
+        }
+
+        void Source_ValueChanged(object sender, string e)
+        {
+            this.Value = e;
+        }
     }
 }
